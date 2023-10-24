0ae0df3b6a6e74c42276bf0c28ac6e53ae4d194f
hirata0802 <148735103+hirata0802@users.noreply.github.com>
2023-10-24 11:41:55 +0900

    Add files via upload

---
+++ &cosme/menu.php
@@ -0,0 +1,25 @@
+<nav>
+    <ul>
+        <li><h1>&cosme</h1></li>
+        <li>
+            <img src="image/home_black.svg" onclick="location.href='.html'" width="40" height="40" alt="home">
+            <div><font size="1">&nbsp;ホーム　　</font></div>
+        </li>
+        <li>
+            <img src="image/search.svg" onclick="location.href='.html'" width="40" height="40" alt="search">
+            <div><font size="1">　検索</font></div>
+        </li>
+        <li>  
+            <img src="image/favorite.svg" onclick="location.href='.html'" width="40" height="40" alt="favorite">
+            <div><font size="1">お気に入り</font></div>
+        </li>
+        <li>
+            <img src="image/cart.svg" onclick="location.href='.html'" width="40" height="40" alt="cart">
+            <div><font size="1">&nbsp;カート</font></div>
+        </li>
+        <li>
+            <img src="image/mypage.svg" onclick="location.href='.html'" width="40" height="40" alt="mypage">
+            <div><font size="1">マイページ</font></div>
+        </li>
+    </ul>
+</nav>
