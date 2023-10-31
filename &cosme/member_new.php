977dd39d4ab8e01cf17d3990966dde977bdf7075
saekisora <148735028+saekisora@users.noreply.github.com>
2023-10-31 11:10:29 +0900

    住所検索機能追加

--- &cosme/member_new.php
+++ &cosme/member_new.php
@@ -13,10 +13,10 @@
         <h2>新規会員登録</h2>
         <p><input type="text" name="sei" placeholder="氏名（姓）"><input type="text" name="mei" placeholder="氏名（名）"></p>
         <p><input type="text" name="seikana" placeholder="かな（せい）"><input type="text" name="meikana" placeholder="かな（めい）"></p>
-        <p><input type="text" name="nikkuname" placeholder="ニックネーム"></p>
-        <p><input type="text" name="yubin" placeholder="郵便番号">
+        <p><input type="text" name="nickname" placeholder="ニックネーム"></p>
+        <p><input type="text" name="zipcode" id="zipcode" placeholder="郵便番号">
         <!---↓検索ボタンで住所自動入力-->
-        <button type="submit" class="ao" id="btn">検索</button>
+        <button type="button" class="ao" id="btn">検索</button>
 
         <p><input type="text" name="prefecture" id="prefecture" placeholder="都道府県"></p>
         <p><input type="text" name="city" id="city" placeholder="市区町村"></p>
@@ -28,7 +28,7 @@
     
         <button type="submit" class="ao">確認</button></p>
     </form>
-    <script src="js/jquery-3.7.0.min.js"></script>
-    <script src="js/app.js"></script>
+    <script src="./js/jquery-3.7.0.min.js"></script>
+    <script src="./js/app.js"></script>
 </body>
 </html>
