596cb9ef4ee446445df8921e7c04fe7021e33186
saekisora <148735028+saekisora@users.noreply.github.com>
2023-10-31 10:13:31 +0900

    menu.phpのリンクを修正

--- &cosme/menu.php
+++ &cosme/menu.php
@@ -2,23 +2,23 @@
     <ul>
         <li><h1>&cosme</h1></li>
         <li>
-            <img src="image/home_black.svg" onclick="location.href='.html'" width="40" height="40" alt="home">
+            <img src="image/home_black.svg" onclick="location.href='home.html'" width="40" height="40" alt="home">
             <div><font size="1">&nbsp;ホーム　　</font></div>
         </li>
         <li>
-            <img src="image/search.svg" onclick="location.href='.html'" width="40" height="40" alt="search">
+            <img src="image/search.svg" onclick="location.href='seach_input.html'" width="40" height="40" alt="search">
             <div><font size="1">　検索</font></div>
         </li>
         <li>  
-            <img src="image/favorite.svg" onclick="location.href='.html'" width="40" height="40" alt="favorite">
+            <img src="image/favorite.svg" onclick="location.href='favorite.html'" width="40" height="40" alt="favorite">
             <div><font size="1">お気に入り</font></div>
         </li>
         <li>
-            <img src="image/cart.svg" onclick="location.href='.html'" width="40" height="40" alt="cart">
+            <img src="image/cart.svg" onclick="location.href='cart.html'" width="40" height="40" alt="cart">
             <div><font size="1">&nbsp;カート</font></div>
         </li>
         <li>
-            <img src="image/mypage.svg" onclick="location.href='.html'" width="40" height="40" alt="mypage">
+            <img src="image/mypage.svg" onclick="location.href='mypage.html'" width="40" height="40" alt="mypage">
             <div><font size="1">マイページ</font></div>
         </li>
     </ul>
