012fa64174583841d4bf805f497679bb9990dcca
saekisora <148735028+saekisora@users.noreply.github.com>
2023-11-06 12:46:40 +0900

    検索画面の修正

--- &cosme/seach_input.php
+++ &cosme/seach_input.php
@@ -7,40 +7,42 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
 </head>
 <body>
-    <!--menu.phpをつなぐ-->
+    <?php require 'menu.php'; ?>
     <form action="seach_output.php" method="post">
         <input type="text" name="keyword" placeholder="キーワードで検索">
         <button type="submit" class="fas fa-search"></button>
-    <h3>カテゴリ</h3>
     <div id="seach">
         <table style="font-size:5pt">
-            <tr><!--3項目ずつ表示-->
-                <td><input type="image" src="./image/eye.jpg" alt="アイシャドウ" width="60px"><br>
-                    アイシャドウ</td>
-                <td><input type="image" src="./image/eyeliner.jpg" alt="アイライナー" width="60px"><br>
-                    アイライナー</td>
-                <td><input type="image" src="./image/eyeliner.jpg" alt="アイライナー" width="60px"><br>
-                    アイライナー</td>
-            </tr>
             <tr>
-                <td><input type="image" src="./image/eye.jpg" alt="アイシャドウ" width="60px"><br>
-                    アイシャドウ</td>
-                <td><input type="image" src="./image/eyeliner.jpg" alt="アイライナー" width="60px"><br>
-                    アイライナー</td>
-                <td><input type="image" src="./image/eyeliner.jpg" alt="アイライナー" width="60px"><br>
-                    アイライナー</td>
+                <th colspan=3>カテゴリー</th>
             </tr>
+            <?php
+            echo '<tr>';
+                echo '<td><input type="image" src="image/eye.jpg" alt="アイシャドウ" width="60px"><br>アイシャドウ</td>';
+                echo '<td><input type="image" src="image/eyeliner.jpg" alt="アイライナー" width="60px"><br>アイライナー</td>';
+                echo '<td><input type="image" src="image/eyeliner.jpg" alt="アイライナー" width="60px"><br>アイライナー</td>';
+            echo '</tr><tr>';
+                echo '<td><input type="image" src="image/eye.jpg" alt="アイシャドウ" width="60px"><br>アイシャドウ</td>';
+                echo '<td><input type="image" src="image/eyeliner.jpg" alt="アイライナー" width="60px"><br>アイライナー</td>';
+                echo '<td><input type="image" src="image/eyeliner.jpg" alt="アイライナー" width="60px"><br>アイライナー</td>';
+            echo '</tr>';
+            ?>
         </table>
-        <h3>ブランド</h3>
         <table style="font-size:5pt">
-            <tr><!--3項目ずつ表示-->
-                <td><input type="image" src="./image/canmake.jpg" alt="キャンメイク" width="60px"><br>
-                キャンメイク</td>
-                <td><input type="image" src="./image/canmake.jpg" alt="キャンメイク" width="60px"><br>
-                    キャンメイク</td>
-                <td><input type="image" src="./image/canmake.jpg" alt="キャンメイク" width="60px"><br>
-                    キャンメイク</td>
+            <tr>
+                <th colspan=3>ブランド</th>
             </tr>
+            <?php
+            echo '<tr>';
+                echo '<td><input type="image" src="image/canmake.jpg" alt="キャンメイク" width="60px"><br>キャンメイク</td>';
+                echo '<td><input type="image" src="image/canmake.jpg" alt="キャンメイク" width="60px"><br>キャンメイク</td>';
+                echo '<td><input type="image" src="image/canmake.jpg" alt="キャンメイク" width="60px"><br>キャンメイク</td>';
+            echo '</tr><tr>';
+                echo '<td><input type="image" src="image/canmake.jpg" alt="キャンメイク" width="60px"><br>キャンメイク</td>';
+                echo '<td><input type="image" src="image/canmake.jpg" alt="キャンメイク" width="60px"><br>キャンメイク</td>';
+                echo '<td><input type="image" src="image/canmake.jpg" alt="キャンメイク" width="60px"><br>キャンメイク</td>';
+            echo '</tr>';
+            ?>
         </table>
     </div>
     </form>
