103ebc60cf5b5c403db0fcb3c50394b6eef39be9
hirata0802 <148735103+hirata0802@users.noreply.github.com>
2023-11-07 11:31:03 +0900

    Update cart.php

--- &cosme/cart.php
+++ &cosme/cart.php
@@ -4,12 +4,29 @@
 <?php require 'menu.php'; ?>
 <?php require 'db_connect.php'; ?>
 <h2>カート</h2>
+<form action="order.html" method="post">
 <?php
     $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('select * from Cart where member_code=?');
-    $sql->execute([$session['customer']['id']]);
-
-<form action="order.html" method="post">
+    $sql=$pdo->prepare('select * from Cart where member_code=? and delete_flag=0 order by id desc');
+    $sql->execute([$_SESSION['customer']['id']]);
+    
+    if($sql->fetch() == true){
+        foreach($sql as $row){
+            $sql2=$pdo->prepare('select * from Cart as C inner join Cosmetics as CO on C.cosme_id=CO.cosme_id inner join Brands as B on CO.brand_id=B.brand_id where member_code=? and delete_flag=0');
+            $sql2->execute([$_SESSION['customer']['id']]);
+            foreach($sql as $row){
+                echo '<img src="', $row['CO.image_path'], '">';
+                echo '<p>ブランド名：', $row['B.brand_name'], '</p>';
+                echo '<p>商品名：', $row['CO.cosme_name'], '</p>';
+                echo '<p>価格：', $row['CO.price'], '</p>';
+                echo '<p>カラー：', $row['CO.color_name'];
+                echo '個数：', $row['C.quantity'], '</p>';
+                echo '<input type="submit" value="削除">';
+            }
+        }
+    }
+    
+    //CO.image_path, B.brand_name, CO.cosme_name, CO.price, CO.color_name, C.quantity
     <img src="https://placehold.jp/120x100.png">
     <p>ブランド名：〇〇〇〇</p>
     <p>商品名：〇〇〇〇</p>
@@ -54,5 +71,6 @@
 
         <p>合計金額:〇〇〇〇</p>
         <p><div class="ao"><button type="submit">レジに進む</button></div></p>
-    </form>
+?>
+</form>
 <?php require 'footer.php'; ?>
