59a7f2c2756b4cf3bf8cb48dd2a0b10d0c84aa2c
0422hana <2201369@s.asojuku.ac.jp>
2023-11-09 09:40:56 +0900

    Update login.php

--- &cosme/login.php
+++ &cosme/login.php
@@ -1,21 +1,52 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログイン画面</title>
-</head>
-<body>
-    <h3>&cosme</h3>
-    <hr>
-    <h2>ログイン</h2>
-    <form action="login_finish.php" method="post">
-        <p><input type="text" name="mail" placeholder="メールアドレス"></p>
-        <p><input type="password" name="pass" placeholder="パスワード"></p>
-        <p><button type="submit">ログイン</button></p>
-    </form>
-    <hr>
-    <p>アカウントをお持ちでない方はこちら</p>
-    <a href="member_new.php">新規会員登録</a>
-</body>
-</html>
+<?php session_start(); ?>
+<?php require 'db_connect.php'; ?>
+<?php
+$msg;
+if(isset($_POST['login'])){
+    unset($_SESSION['customer']);
+    
+    $pdo = new PDO($connect, USER, PASS);
+    $sql = $pdo -> prepare('select * from Members where email=? and member_password=?');
+    $sql -> execute([$_POST['mail'],$_POST['pass']]);
+    
+    foreach($sql as $row){
+        //if($_POST['pass']==$row['member_password']){
+            $_SESSION['customer'] = [
+                'code' => $row['member_code'],
+                'familyName' => $row['family_name'],
+                'firstName' => $row['first_name'],
+                'familyKana' => $row['family_name_kana'],
+                'firstKana' => $row['first_name_kana'],
+                'post' => $row['post_code'],
+                'address' => $row['address'],
+                'phone' => $row['phone'],
+                'mail' => $row['email'],
+                'pass' => $row['member_password']];
+            }
+    
+    
+    if(isset($_SESSION['customer'])){
+        header('Location: ./home.php');
+        exit();
+    }else{
+        $msg = 'ログイン名またはパスワードが違います';
+    }
+}
+require 'header.php';
+echo '<h3>&cosme</h3>';
+echo '<hr>';
+echo '<h2>ログイン</h2>';
+if(isset($msg)){
+echo '<p>', $msg, '</p>';
+}
+
+echo '<form action="login.php" method="post">';
+echo '<p><input type="text" name="mail" placeholder="メールアドレス"></p>';
+echo  '<p><input type="password" name="pass" placeholder="パスワード"></p>';
+echo  '<p><input name="login" type="submit" value="ログイン"></p>';
+echo '</form>';
+echo '<hr>';
+echo '<p>アカウントをお持ちでない方はこちら</p>';
+echo '<a href="member_new.php">新規会員登録</a>';
+?>
+<?php require 'footer.php'; ?>
