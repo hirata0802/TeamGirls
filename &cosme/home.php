ca69af731335c1872b0fe83c604416cc04577b3c
0422hana <2201369@s.asojuku.ac.jp>
2023-11-09 09:39:17 +0900

    変更

--- &cosme/home.php
+++ &cosme/home.php
@@ -4,21 +4,20 @@
 <?php require 'menu.php'; ?>
 
     <h2>今週のランキング</h2>
-
+    <table>
 <?php
-if(isset($_SESSION['customer'])){
     //今週のランキング;
     $pdo = new PDO($connect, USER, PASS);
-    $sql = $pdo -> query('select * from Cosmetics');
+    $sql = $pdo -> query('select * from Cosmetics order by price desc');
     foreach($sql as $row){
         echo '<tr>';
         echo '<td>', $row['cosme_name'], '</td>';
         echo '<td>', $row['price'], '</td>';
         echo '</tr>';
     }
-    //新作情報;
-}
+    //新作情報;}
 ?>
+</table>
 </body>
 </html>
 <?php
