2b368fb2d012ea238a3220ad953946485c264e63
saekisora <148735028+saekisora@users.noreply.github.com>
2023-11-13 15:03:19 +0900

    kensakukekka

--- &cosme/seach_input.php
+++ &cosme/seach_input.php
@@ -1,18 +1,11 @@
+<?php require 'header.php'; ?>
 <?php require 'db_connect.php'; ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>seach</title>
-</head>
-<body>
-    <?php require 'menu.php'; ?>
+<?php require 'menu.php'; ?>
     <form action="seach_output.php" method="post">
         <input type="text" name="keyword" placeholder="キーワードで検索" >
         <input type="image" src="./image/seach.jpg" alt="検索" width="25px" formaction="seach_output.php?kubun=3&id=key'">
         <?php
-        echo '<table style="font-size:5pt">';
+        echo '<table width="100%">';
             echo '<tr>';
                 echo '<th colspan=3>カテゴリー</th>';
             echo '</tr>';
@@ -23,15 +16,15 @@
             foreach($sql as $row){
                 $category_id=$row['category_id'];
                 if($count%3!=0){
-                    echo '<td><input type="image" src="',$row['image_path'],'" alt="',$category_id,'" width="60px" formaction="seach_output.php?kubun=1&id=',$category_id,'"><br>',$row['category_name'],'</td>';
+                    echo '<td align="center"><input type="image" src="',$row['image_path'],'" alt="',$category_id,'" width="100px" formaction="seach_output.php?kubun=1&id=',$category_id,'"><br>',$row['category_name'],'</td>';
                 }else{
-                    echo '<td><input type="image" src="',$row['image_path'],'" alt="',$category_id,'" width="60px" formaction="seach_output.php?kubun=1&id=',$category_id,'"><br>',$row['category_name'],'</td>';
+                    echo '<td align="center"><input type="image" src="',$row['image_path'],'" alt="',$category_id,'" width="100px" formaction="seach_output.php?kubun=1&id=',$category_id,'"><br>',$row['category_name'],'</td>';
                     echo '</tr><tr>';
                 }
                 $count++;
             }
         echo '</table>';
-        echo '<table style="font-size:5pt">';
+        echo '<table width="100%">';
             echo '<tr>';
                 echo '<th colspan=3>ブランド</th>';
             echo '</tr>';
@@ -42,9 +35,9 @@
             foreach($sql as $row){
                 $brand_id=$row['brand_id'];
                 if($count%3!=0){
-                    echo '<td><input type="image" src="',$row['image_path'],'" alt="',$brand_id,'" width="60px" formaction="seach_output.php?kubun=2&id=',$brand_id,'"><br>',$row['brand_name'],'</td>';
+                    echo '<td align="center"><input type="image" src="',$row['image_path'],'" alt="',$brand_id,'" width="100px" formaction="seach_output.php?kubun=2&id=',$brand_id,'"><br>',$row['brand_name'],'</td>';
                 }else{
-                    echo '<td><input type="image" src="',$row['image_path'],'" alt="',$brand_id,'" width="60px" formaction="seach_output.php?kubun=2&id=',$brand_id,'"><br>',$row['brand_name'],'</td>';
+                    echo '<td align="center"><input type="image" src="',$row['image_path'],'" alt="',$brand_id,'" width="100px" formaction="seach_output.php?kubun=2&id=',$brand_id,'"><br>',$row['brand_name'],'</td>';
                     echo '</tr><tr>';
                 }
                 $count++;
@@ -52,5 +45,4 @@
         echo '</table>';
         ?>
     </form>
-</body>
-</html>
+<?php require 'footer.php'; ?>
--- &cosme/seach_output.php
+++ &cosme/seach_output.php
@@ -1,13 +1,6 @@
+<?php require 'header.php'; ?>
 <?php require 'db_connect.php'; ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>seach-output</title>
-</head>
-<body>
-    <?php require 'menu.php'; ?>
+<?php require 'menu.php'; ?>
     <button onclick="history.back()">＜戻る</button>
     <hr>
     <?php
@@ -17,13 +10,11 @@
         $sql=$pdo->prepare('select * from Cosmetics C inner join Brands B on C.brand_id = B.brand_id where C.category_id = ?');
         $sql->execute([$_GET['id']]);
         $count=$sql->rowCount();
-        echo 'a';
     }else if($_GET['kubun']==2){
         //ブランド
         $sql=$pdo->prepare('select * from Cosmetics C inner join Brands B on C.brand_id = B.brand_id where C.brand_id = ?');
         $sql->execute([$_GET['id']]);
         $count=$sql->rowCount();
-        echo 'b';
     }else if($_GET['kubun']==3){
         $sql=$pdo->prepare('select * from Cosmetics C inner join Brands B on C.brand_id = B.brand_id where C.cosme_name like ?');
         $sql->execute(['%'.$_POST['keyword'].'%']);
@@ -32,28 +23,29 @@
         $sql=$pdo->query('select * from Cosmetics C inner join Brands B on C.brand_id = B.brand_id');
         $count=$sql->rowCount();
     }
-    echo '<table>';
-        echo '<th colspan="2">',$count,'件</th><th align="right"><button type="button">絞り込み</button></th>';
+    echo '<table width="100%">';
+        echo '<th align="left">',$count,'件</th>';
+        echo '<th align="right"><button type="button">絞り込み</button></th>';
         echo '<form action="detail.php" method="post">';
             $rowcount=1;
             echo '<tr>';
             foreach($sql as $row){
                 if($rowcount%2!=0){
                     echo '<td>';
-                        echo '<table>';
+                        echo '<table width="80%">';
                             echo '<tr><td colspan="3"align="center"><input type="image" src="',$row['image_path'],'" alt="',$row['cosme_name'],'" width="150px" height="150px"></td></tr>';
+                            echo '<tr><td colspan="2" align="left" white-space: nowrap>',$row['cosme_name'],'</td><td align="right"><a href="#">☆</a></td><tr>';
                             echo '<tr><td colspan="3" align="left">',$row['brand_name'],'</td></tr>';
-                            echo '<tr><td colspan="2" align="left">',$row['cosme_name'],'</td><td><a href="#">☆</a></td><tr>';
-                            echo '<tr><td>',$row['price'],'</td><td colspan=2 align="right"><a href="#">カートに入れる</a></td></tr>';
+                            echo '<tr><td width="30%">',$row['price'],'</td><td colspan="2" align="right"><a href="#">カートに入れる</a></td></tr>';
                         echo '</table>';
                     echo '</td>';
                 }else{
                     echo '<td>';
-                        echo '<table>';
+                        echo '<table width="100%">';
                             echo '<tr><td colspan="3"align="center"><input type="image" src="',$row['image_path'],'" alt="',$row['cosme_name'],'" width="150px" height="150px"></td></tr>';
+                            echo '<tr><td colspan="2" align="left" white-space: nowrap>',$row['cosme_name'],'</td><td align="right"><a href="#">☆</a></td></tr>';
                             echo '<tr><td colspan="3" align="left">',$row['brand_name'],'</td></tr>';
-                            echo '<tr><td colspan="2" align="left">',$row['cosme_name'],'</td><td><a href="#">☆</a></td></tr>';
-                            echo '<tr><td>',$row['price'],'</td><td colspan=2 align="right"><a href="#">カートに入れる</a></td></tr>';
+                            echo '<tr><td width="30%">',$row['price'],'</td><td colspan="2" align="right"><a href="#">カートに入れる</a></td></tr>';
                         echo '</table>';
                     echo '</td>';
                     echo '</tr><tr>';
@@ -64,5 +56,4 @@
     echo '</form>';
     
     ?>
-</body>
-</html>
+<?php require 'footer.php'; ?>
