dfff6fbc655e30e3c4ec62ee4531241de0d9c66f
0422hana <2201369@s.asojuku.ac.jp>
2023-11-14 17:21:51 +0900

    Update detail.php

--- &cosme/detail.php
+++ &cosme/detail.php
@@ -1,23 +1,31 @@
 <?php session_start(); ?>
 <?php require 'db_connect.php'; ?>
 <?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
+<?php //require 'menu.php'; ?>
 
 <button onclick="history.back()">＜戻る</button>
 
 <?php
-    echo '<h3>',$_POST['cosme_name'],'</h3>';
+    $color = ['レッド', 'オレンジ', 'ピンク', 'ベージュ', '白', 'ブラウン', 'ブラック', 'シルバー', 'ゴールド', 'その他'];
 
-//商品詳細表示
-    echo '<div class="out">';
-    echo    '<div class="in">';
-    $counts = 1;
-        foreach($counts==1){ //カラー取得
-            '<label><input type=radio name="slide" checked><span></span><a href="#"><img src="',$_POST['image_path'],'" width="200"></a></label>';
-        }
+    $pdo = new PDO($connect, USER, PASS);
 
-        echo '<p>販売価格:',$_POST['price'],'</p>';
-        echo '<p>カラー:',$_POST['color_id'],'</p>';
+     echo '<h3>',$_POST['cosme_name'],'</h3>';
+// //商品詳細表示
+     echo '<div class="out">';
+     echo    '<div class="in">';
+     $cosme = $pdo -> prepare('select * from Cosmetics where group_id=? and brand_id=? and category_id=?');
+     $cosme -> execute([$_POST['group_id'], $_POST['brand_id'], $_POST['category_id']]);
+     $cosmeCount = $cosme -> rowCount();
+
+     //foreach($cosme as $row){ //カラー取得
+       //  if($cosmeCount == 0){
+             //'<label><input type=radio name="slide" checked><span></span><a href="#"><img src="',$_row['image_path'],'" width="200"></a></label>';
+         //}
+         //$cosmeCount--;
+     //}
+    echo '<p>販売価格:',$cosme['price'],'</p>';
+    echo '<p>カラー:',$color['color_id'],'</p>';
     echo '</div>';
     echo '</div>';
 
@@ -25,7 +33,7 @@
     echo '<td><a href="cart.php?cosmeId=',$cosmeId,'"><button>カートに入れる</button></a></td>'; 
     echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'">　　　★</a></td></tr>';
     echo '<p>商品詳細</p>';
-    echo '<p>',$_POST['cosme_ex'],'</p>';
+    echo '<p>',$cosme['cosme_ex'],'</p>';
     echo '<p><strong>レビュー</strong></p>';
 
 //レビュー評価★
@@ -39,7 +47,6 @@
     echo '</form>';
 
 //レビュー表示
-    $pdo = new PDO($connect, USER, PASS);
     $review = $pdo -> prepare('select * from Reviews as R join Mypages as M on R.cosme_id = M.cosme_id where cosme_name = ?');
     $review -> execute([$_POST['cosme_name']]);
     $count = 1;
