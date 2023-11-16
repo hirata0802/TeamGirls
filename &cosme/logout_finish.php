4a6aaaa1316d180d5e0b302695c31a970d4c9410
ShikimachiMinori <148735025+ShikimachiMinori@users.noreply.github.com>
2023-11-16 11:45:15 +0900

    画像の追加

--- &cosme/logout_finish.php
+++ &cosme/logout_finish.php
@@ -4,16 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログアウト画面</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>ログイン画面</title>
 </head>
 <body>
-    <h3>&cosme</h3>
-    <hr>
+    <h1>&cosme</h1>
+    <hr color="black">
     <?php
 if(isset($_SESSION['customer'])){
     unset($_SESSION['customer']);
+    echo '<div id="logtitle3">';
     echo 'ログアウトしました。';
+    echo '</div>';
     echo '<form action="login.php" method="post">';
+    echo '<hr width="250">';
     echo '<button class="ao">ログイン画面へ戻る</button>';
     echo '</form>';//変更
    
