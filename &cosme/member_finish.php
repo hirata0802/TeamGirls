fd980ff198b513d5d37b9801c5a5b5b506bc2964
fukui12 <2201452@s.asojuku.ac.jp>
2023-11-16 10:50:37 +0900

    新規登録

--- &cosme/member_finish.php
+++ &cosme/member_finish.php
@@ -1,18 +1,10 @@
 <?php session_start(); ?>
+<?php require 'header.php'; ?>
 <?php require 'db_connect.php'; ?>
 <?php
 $pdo=new PDO($connect,USER,PASS);
 
-// if(isset($_SESSION['customer'])){
-//     $id=$_SESSION['customer']['id'];
-//     $sql=$pdo->prepare('select * from Members where id!=? and login=?');
-//     $sql->execute([$id, $_POST['login']]);
-// }else{
-//     $sql=$pdo->prepare('select * from Members where login=?');
-//     $sql->execute([$_POST['login']]);
-// }
-// if(empty($sql->fetchAll())){
-    if(!isset($_SESSION['customer'])){
+   if(!isset($_SESSION['customer'])){
         $ads=$_POST['prefecture'].$_POST['city'].$_POST['address'].$_POST['bill'];
             $sql=$pdo->prepare('insert into Members values(null,?,?,?,?,?,?,?,?,?)');
             $sql->execute([
@@ -20,16 +12,25 @@ $pdo=new PDO($connect,USER,PASS);
                 $_POST['seikana'],$_POST['meikana'],
                 $_POST['zipcode'],
                 $ads,$_POST['tel'],$_POST['mail'],
-                $_POST['pass']
- 
-             ]);
-             $sql2=$pdo->prepare('insert into Mypage (member_nickname) values(?)');
-             $sql2->execute([$_POST['nickname']]);
-            echo 'お客様情報を登録しました。';
-        // }else{
-            // echo '会員情報がすでに登録されていますので、変更してください。';
+                $_POST['pass']]);
+
+             $id=$pdo->lastInsertId();
+             $sql2=$pdo->prepare('insert into Mypage (member_code,member_nickname) values(?,?)');
+             $sql2->execute([$id,$_POST['nickname']]);
+            echo '<h3>&cosme</h3>';
+            echo '<hr>';
+            echo '<h2>登録完了</h2>';
+            echo '<p><font color="FF0000">',$_POST['sei'],$_POST['mei'],'様</font></p>';
+            echo '<p><font color="FF0000">会員登録ありがとうございます。</font></p>';
+            echo '<form action="home.php" method="post">';
+            echo '<button class="ao">ホームへ</button>';
+            echo '<hr>';
+            echo '<p>登録情報を確認・変更できます</p>';
+            echo '<a href="mypage.php">＞マイページへ</a>';
+           
+
         }
-// }
+
 ?>
 <?php require 'footer.php'; ?>
  
