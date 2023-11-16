5570886cf8e2e3ab66e3b5ced44d3b60954fad0b
hirata0802 <148735103+hirata0802@users.noreply.github.com>
2023-11-16 10:54:39 +0900

    a

--- &cosme/mypage.php
+++ &cosme/mypage.php
@@ -2,4 +2,57 @@
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php require 'db_connect.php'; ?>
+<?php
+    $pdo=new PDO($connect, USER, PASS);
+    $sql=$pdo->prepare('select * from Mypage where member_code=?');
+    $sql->execute([$_SESSION['customer']['code']]);
+
+    echo '<p>ニックネーム</p>';
+    echo '<p><input type="text" name="nickname" value="', $row['member_nickname'], '"></p>';
+    
+    //佐伯のラベルを付け加える
+    echo '<p>年代</p>';
+    echo '<select name="age">';
+    if($i<60){
+        echo '<option value="', $row['member_age'], '" selected hidden>', $row['member_age'], '代</option>';
+    }else{
+        echo '<option value="', $row['member_age'], '" selected hidden>', $row['member_age'], '代以上</option>';
+    }
+    for($i=10; $i<=60; $i+=10){
+        if($i<60){
+            echo '<option value="', $i, '">', $i, '代</option>';
+        }else{
+            echo '<option value="', $i, '">', $i, '代以上</option>';
+        }
+    }        
+    echo '</select>';
+    
+    echo '<p>性別</p>';
+    echo '<select name="sei">';
+    echo '<option value="', $row['member_gender'], '" selected hidden>', $row['member_gender'], '</option>';
+    echo '<option value="女性">女性</option>';
+    echo '<option value="男性">男性</option>';
+    echo '<option value="その他">その他</option>';
+    echo '</select>';
+    
+    echo '<p>肌質</p>';
+    echo '<select name="skin">';
+    echo '<option value="', $row['member_skin'], '" selected hidden>', $row['member_skin'], '</option>';
+    echo '<option value="普通肌">普通肌</option>';
+    echo '<option value="乾燥肌">乾燥肌</option>';
+    echo '<option value="脂性肌">脂性肌</option>';
+    echo '<option value="混合肌">混合肌</option>';
+    echo '<option value="敏感肌">敏感肌</option>';
+    echo '</select>';
+    
+    echo '<p>パーソナルカラー</p>';
+    echo '<select name="p_color">';
+    echo '<option value="', $row['member_color'], '" selected hidden>', $row['member_color'], '</option>';
+    echo '<option value="イエベ">イエベ</option>';
+    echo '<option value="ブルベ">ブルベ</option>';
+    echo '</select>';
+
+
+
+?>
 <?php require 'footer.php'; ?>
--- &cosme/order_db_insert.php
+++ &cosme/order_db_insert.php
@@ -1,12 +1,19 @@
 <?php require 'db_connect.php'; ?>
 <?php
     $pdo=new PDO($connect, USER, PASS);
-    $ads=$_POST['prefecture'].$_POST['city'].$_POST['address'].$_POST['bill'];
-    $sql=$pdo->prepare('insert into Addresses values (null,?,?,?,?,?,CURRENT_DATE)');
-    $sql->execute([
-        $_SESSION['customer']['code'],
-        $_POST['name'],
-        $_POST['zipcode'],
-        $ads,
-        $_POST['tel'],
-    ]);
+    echo $order;
+    /*if($order == 0){
+        $ads=$_POST['prefecture'].$_POST['city'].$_POST['address'].$_POST['bill'];
+        $sql=$pdo->prepare('insert into Addresses values (null,?,?,?,?,?,CURRENT_DATE)');
+        $sql->execute([
+            $_SESSION['customer']['code'],
+            $_POST['name'],
+            $_POST['zipcode'],
+            $ads,
+            $_POST['tel'],
+        ]);
+
+    }else if($order == 1){
+        
+    }*/
+?>
