4f27f4404ac8dc4e9eb9205954f64978b810a6fd
0422hana <2201369@s.asojuku.ac.jp>
2023-11-17 13:50:18 +0900

    お気に入り、商品詳細

--- &cosme/favorite_show.php
+++ &cosme/favorite_show.php
@@ -1,7 +1,17 @@
 <?php session_start(); ?>
 <?php require 'db_connect.php'; ?>
 <?php //require 'menu.php'; ?>
-<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/detail.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
+<title>&cosme</title>
+</head>
+<body>
 
 <?php
     if(isset($_SESSION['customer'])){
@@ -17,7 +27,8 @@
         echo '<p>',$count,'件  <button class="ao" onclick="location.href=\'serch_input.php\'">絞り込み</button></p>';
     
         //表示
-        $sql = $pdo -> prepare('select * from Cosmetics as C inner join Favorites as F on C.cosme_id=F.cosme_id inner join Brands as B on C.brand_id=B.brand_id where F.member_code=? and delete_flag=0');
+        $sql = $pdo -> prepare('select * from Cosmetics as C inner join Favorites as F on C.cosme_id=F.cosme_id inner join Brands as B on C.brand_id=B.brand_id where F.member_code=?');
+        //$sql = $pdo -> prepare('select * from Cosmetics as C inner join Favorites as F on C.cosme_id=F.cosme_id inner join Brands as B on C.brand_id=B.brand_id where F.member_code=? and delete_flag=0');
         $sql -> execute([$_SESSION['customer']['code']]);
         foreach($sql as $row){
             echo '<table>';
@@ -33,7 +44,12 @@
 
             echo '<tr>';
             echo '<td>',$row['cosme_name'],'</td>';
-            echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'">　　　★</a></td>';
+            //お気に入りボタン設定
+            if($row['delete_flag']==0){
+                echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'">★</a></td>';
+            }else{
+                echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'">☆</a></td>';
+            }
             echo '</tr>';
 
             echo '<tr>';
@@ -44,5 +60,7 @@
         }
         
     }
-?>
+    ?>
+    <script src="./js/favorite.js"></script>
+
 <?php require 'footer.php'; ?>
