6eea8e459454a5dca88f5ed3f57f37922b4d8b78
saekisora <148735028+saekisora@users.noreply.github.com>
2023-11-17 12:49:26 +0900

    複数検索機能完成

--- &cosme/seach_input.php
+++ &cosme/seach_input.php
@@ -1,15 +1,73 @@
-<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/style.css">
+<title>&cosme</title>
+</head>
+<body>
 <?php require 'db_connect.php'; ?>
 <?php require 'menu.php'; ?>
     <form action="seach_output.php" method="post">
-        <input type="text" name="keyword" placeholder="キーワードで検索" >
+        <!--↓見えないからbr追加してる-->
+        <br><br><br><br>
+        <input type="text" name="keyword" placeholder="キーワードで検索">
         <input type="image" src="./image/seach.jpg" alt="検索" width="25px" formaction="seach_output.php?kubun=3&id=key'">
+        <hr width="70%">
+        <h4 align="center">複数絞り込み</h4>
+        <table width="70%" align="center">
+            <tr>
+                <th>カテゴリー</th><th>ブランド</th><th>カラー</th>
+            </tr>
+            <?php
+            $pdo=new PDO($connect, USER, PASS);
+            echo '<tr>';
+                echo '<td align="center">';
+                    echo '<label class="selectbox">';
+                        $sql=$pdo->query('select * from Categories');
+                        echo '<select name="categorySelect">';
+                            echo '<option value="0">選択しない</option>';
+                            foreach($sql as $row){
+                                echo '<option value="',$row['category_id'],'">',$row['category_name'],'</option>';
+                            }
+                        echo '</select>';
+                    echo '</label>';
+                echo '</td>';
+                echo '<td align="center">';
+                    echo '<label class="selectbox">';
+                        $sql=$pdo->query('select * from Brands');
+                        echo '<select name="brandSelect">';
+                            echo '<option value="0">選択しない</option>';
+                            foreach($sql as $row){
+                                echo '<option value="',$row['brand_id'],'">',$row['brand_name'],'</option>';
+                            }
+                        echo '</select>';
+                    echo '</label>';
+                echo '</td>';
+                echo '<td align="center">';
+                    echo '<label class="selectbox">';
+                    $color=[1=>'レッド', 2=>'オレンジ', 3=>'ピンク', 4=>'ベージュ', 5=>'ホワイト', 6=>'ブラウン', 7=>'ブラック', 8=>'シルバー', 9=>'ゴールド', 10=>'その他'];
+                        echo '<select name="colorSelect">';
+                            echo '<option value="0">選択しない</option>';
+                            foreach($color as $key => $value){
+                                echo '<option value="',$key,'">',$value,'</option>';
+                            }
+                        echo '</select>';
+                    echo '</label>';
+                echo '</td>';
+                ?>
+            </tr>
+            <tr>
+                <td colspan="3"><br><button type="submit" class="ao" name="multiseach">検索</button></td>
+            </tr>
+        </table>
+        <hr width="70%">
         <?php
         echo '<table width="100%">';
             echo '<tr>';
-                echo '<th colspan=3>カテゴリー</th>';
+                echo '<th colspan="3">カテゴリー</th>';
             echo '</tr>';
-            $pdo=new PDO($connect, USER, PASS);
             $sql=$pdo->query('select * from Categories');
             $count=1;
             echo '<tr>';
@@ -26,7 +84,7 @@
         echo '</table>';
         echo '<table width="100%">';
             echo '<tr>';
-                echo '<th colspan=3>ブランド</th>';
+                echo '<th colspan="3">ブランド</th>';
             echo '</tr>';
 
             $sql=$pdo->query('select * from Brands');
@@ -42,7 +100,7 @@
                 }
                 $count++;
             }
-        echo '</table>';
         ?>
+        </table>
     </form>
 <?php require 'footer.php'; ?>
