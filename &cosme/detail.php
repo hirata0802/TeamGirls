6f30d592989831db6c71d5ff6609922b550044f8
0422hana <2201369@s.asojuku.ac.jp>
2023-11-21 10:20:55 +0900

    詳細、お気に入り

--- &cosme/detail.php
+++ &cosme/detail.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <?php require 'menu.php'; ?>
-<br><br><br>
+<br><br>
 <button onclick="history.back()">＜戻る</button>
 
 <?php
@@ -20,31 +20,44 @@
     $cosme1 -> execute([$_GET['cosme_id'], $_GET['group_id'], $_GET['brand_id'], $_GET['category_id']]);
     $count = 1;
 
-    echo '<div class="out">';
-    echo '<div class="in">';
     foreach($cosme1 as $row){ 
+        $cosmeId = $row['cosme_id'];
         if($count==1){
-            echo '<h3>',$row['cosme_name'],'</h3>';
-            echo '<p>販売価格:',$row['price'],'</p>';
+            echo '<h3 align="center">',$row['cosme_name'],'</h3>';
+            //echo '<p>販売価格:',$row['price'],'</p>';
             $cosmeEx = $row['cosme_detail'];
             $count++;
         }   
-        echo '<label><input type=radio name="slide"><span></span><a href="#"></a><img src="',$row['image_path'],'" width="200"></label>';
+        echo '<div class="out">';
+        echo '<div class="in">';
+        for($a=1; $a<=$count; $a++){
+            echo '<label><input type=radio name="slide" checked><span></span><a href="#"><img src="',$row['image_path'],'" width="200" alt="',$row['color_name'],'"></a></label>';
+        }
+        echo '</div>';
+        echo '</div>';
+        $price = $row['price'];
         $colorName = $row['color_name'];
     }
-    echo $colorName;
-    echo '</div>';
-    echo '</div>';
+    echo '<p>販売価格：￥',$price,'</p>';
+    echo '<p>カラー：',$colorName,'</p>';
 
-    echo '<table><tr>';
-    echo '<td><a href="cart.php?cosmeId=',$cosmeId,'"><button>カートに入れる</button></a></td>';
-    if($row['delete_flag']==0){
-        echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'&page=0">★</a></td>';
-    }else{
-        echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'&page=0">☆</a></td>';
+    //echo '<table><tr>';
+    echo '<p><a href="cart.php?cosmeId=',$cosmeId,'"><button>カートに入れる</button></a></p>';
+
+    $cosme2 = $pdo -> prepare('select * from Favorites as F inner join Cosmetics as C on F.cosme_id = C.cosme_id  where group_id = ? and member_code = ?');
+    $cosme2 -> execute([$_GET['group_id'], $_SESSION['customer']['code']]);
+    $i = 0;
+    foreach($cosme2 as $row){
+        if($i == 0){
+            if($row['delete_flag']==1){
+                echo '<a href="favorite.php?cosmeId=',$cosmeId,'$page=0">☆</a>';
+            }else{
+                echo '<a href="favorite.php?cosmeId=',$cosmeId,'$page=2">★</a>';
+            }
+        }
     }
-    //echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'">　　　★</a></td></tr>';
-    echo '<p>商品詳細</p>';
+
+    echo '<p align="center">商品詳細</p>';
     echo '<p>',$cosmeEx,'</p>';
     echo '<p><strong>レビュー</strong></p>';
 
@@ -59,7 +72,7 @@
 
 //レビュー表示
     $review = $pdo -> prepare('select * from Reviews as R join Mypages as M on R.cosme_id = M.cosme_id where cosme_name = ?');
-    $review -> execute([$_POST['cosme_name']]);
+    $review -> execute([$_GET['cosme_name']]);
     $count = 1;
     if($count<=2){
         foreach($review as $row){
@@ -73,12 +86,3 @@
     echo '<td><a href="review.php"><button>レビューを書く</button></a></td>'; 
 ?>
 <?php require 'footer.php'; ?>
-
-if($row['delete_flag']==0){
-                echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'">★</a></td>';
-                $sql = $pdo -> prepare('insert into Favorites values(?, ?, current_date, 0)');
-                $sql -> execute([$_SESSION['customer']['code'], $_GET['cosmeId']]);
-            }else{
-                
-                echo '<td><a href="favorite.php?cosmeId=',$cosmeId,'">☆</a></td>';
-            }
