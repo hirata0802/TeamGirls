115f31b0733e2b16bb4d4b3d3986b7b6bc4b4f71
ShikimachiMinori <148735025+ShikimachiMinori@users.noreply.github.com>
2023-11-22 13:55:06 +0900

    Update logout.php

--- &cosme/logout.php
+++ &cosme/logout.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
     <title>ログイン画面</title>
 </head>
     <h3>&cosme</h3>
