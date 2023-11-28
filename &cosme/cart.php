d1b7e7ba258a5e9d0393a901ab5c205d82f48248
hirata0802 <148735103+hirata0802@users.noreply.github.com>
2023-11-28 12:46:27 +0900

    あああ

--- &cosme/cart.php
+++ &cosme/cart.php
@@ -1,16 +1,18 @@
 <?php session_start(); ?>
 <?php require 'db_connect.php'; ?>
 <?php
-    $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('select C.cart_id, B.brand_name, CO.cosme_name, CO.price, CO.color_name, CO.image_path, C.quantity from Cart as C inner join Cosmetics as CO on C.cosme_id=CO.cosme_id inner join Brands as B on CO.brand_id=B.brand_id where member_code=? and delete_flag=0');
-    $sql->execute([$_SESSION['customer']['code']]);
-
-    if($sql->rowCount() == 0){
-      echo "レコードが有りません";
-    }else{
-      foreach ($sql as $row) {
-          $data[] = $row;
-      }
-      echo json_encode($data);
+  $pdo=new PDO($connect, USER, PASS);
+  $sql=$pdo->prepare('select * from Cart as C inner join Cosmetics as CO on C.cosme_id=CO.cosme_id inner join Brands as B on CO.brand_id=B.brand_id where member_code=? and delete_flag=0');
+  $sql->execute([$_SESSION['customer']['code']]);
+  //$total=0;
+  if($sql->rowCount() == 0){
+    echo "レコードが有りません";
+  }else{
+    foreach ($sql as $row) {
+      //$total += $row['price'] * $row['quantity'];
+      $data[] = $row;
     }
+    //値をjson形式で出力
+    echo json_encode($data);
+  }
 ?>
