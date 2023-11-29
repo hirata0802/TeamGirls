1b0fc3affac82f17471bdda96b78fe7306da5435
0422hana <2201369@s.asojuku.ac.jp>
2023-11-29 15:09:32 +0900

    a

--- &cosme/detail_next.php
+++ &cosme/detail_next.php
@@ -3,18 +3,36 @@
 <?php
     $pdo = new PDO($connect, USER, PASS);
     $minId = $pdo -> prepare('select min(cosme_id) from Cosmetics where group_id=?');
-    $minId -> execute([$_GET['group_id']]);
+    $minId -> execute([$_GET['group']]);
     $maxId = $pdo -> prepare('select max(cosme_id) from Cosmetics where group_id=?');
-    $maxId -> execute([$_GET['group_id']]);
+    $maxId -> execute([$_GET['group']]);
     if($_GET['next']==0){   //前のcosme_idforeach($minId as $m){
-            if($minId == $_GET['cosme_id']){
-                header('Location: ./detail.php?cosmeId='.$maxId);
-                exit();
-            }else{
-                header('Location: ./detail.php?cosmeId='.$_GET['cosme_id']+1);
-                exit();
-
-            }
+        if($minId == $_GET['cosmeId']){
+            header('Location: ./detail.php?cosme_id='.$maxId);
+            exit();
+        }
+        else if($maxId == $_GET['cosmeId']){
+            header('Location: ./detail.php?cosme_id='.$minId);
+            exit();
+        }
+        else{
+            header('Location: ./detail.php?cosme_id='.$_GET['cosmeId']-1);
+            exit();
+        }
+    }
+    else if($_GET['next']==1){   //前のcosme_idforeach($minId as $m){
+        if($minId == $_GET['cosmeId']){
+            header('Location: ./detail.php?cosme_id='.$maxId);
+            exit();
+        }
+        else if($maxId == $_GET['cosmeId']){
+            header('Location: ./detail.php?cosme_id='.$minId);
+            exit();
+        }
+        else{
+            header('Location: ./detail.php?cosme_id='.$_GET['cosmeId']+1);
+            exit();
+        }
     }
     
 ?>
