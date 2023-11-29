fac5f46a8a268d94b9204d3d529e7b8b39cd983a
hirata0802 <148735103+hirata0802@users.noreply.github.com>
2023-11-29 21:01:51 +0900

    わああ

--- &cosme/mypage.php
+++ &cosme/mypage.php
@@ -39,11 +39,7 @@ if(isset($_POST['nickname']) && isset($_POST['age']) && isset($_POST['sei']) && 
     echo '<div id="mannaka">';
     foreach($sql as $row){
         echo '<p>ニックネーム</p>';
-        if(empty($row['member_nickname'])){
-            echo '<p><input type="text" style="width: 230px;height: 40px;" name="nickname" value="unknown"></p>';
-        }else{
-            echo '<p><input type="text" style="width: 230px;height: 40px;" name="nickname" value="', $row['member_nickname'], '"></p>';
-        }
+        echo '<p><input type="text" style="width: 230px;height: 40px;" name="nickname" value="', $row['member_nickname'], '"></p>';
         
         //佐伯のラベルを付け加える
         echo '<p>年代</p>';
