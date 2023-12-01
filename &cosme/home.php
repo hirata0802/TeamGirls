5c55934521963bfc9450df9613910aa149bf2fe3
ShikimachiMinori <148735025+ShikimachiMinori@users.noreply.github.com>
2023-12-01 12:43:27 +0900

    Update home.php

--- &cosme/home.php
+++ &cosme/home.php
@@ -1,7 +1,31 @@
 <?php session_start(); ?>
 <?php require 'db_connect.php'; ?>
 <?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
+<nav>
+    <ul>
+        <li><h1>&cosme</h1></li>
+        <li>
+            <img src="css/image/home_black.svg" onclick="location.href='home.php'" width="40" height="40" alt="home">
+            <div><font size="1">&nbsp;ホーム　　</font></div>
+        </li>
+        <li>
+            <img src="css/image/search.svg" onclick="location.href='seach_input.php'" width="40" height="40" alt="search">
+            <div><font size="1">　検索</font></div>
+        </li>
+        <li>  
+            <img src="css/image/favorite.svg" onclick="location.href='favorite_show.php'" width="40" height="40" alt="favorite">
+            <div><font size="1">お気に入り</font></div>
+        </li>
+        <li>
+            <img src="css/image/cart.svg" onclick="location.href='cart_show.php'" width="40" height="40" alt="cart">
+            <div><font size="1">&nbsp;カート</font></div>
+        </li>
+        <li>
+            <img src="css/image/mypage.svg" onclick="location.href='mypage.php'" width="40" height="40" alt="mypage">
+            <div><font size="1">マイページ</font></div>
+        </li>
+    </ul>
+</nav>
 
 <table>
 <?php
