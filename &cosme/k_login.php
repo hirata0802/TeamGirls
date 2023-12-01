ea738dab88f48f6749833f3109df4de468a6fb40
fukui12 <2201452@s.asojuku.ac.jp>
2023-12-01 10:53:22 +0900

    a

---
+++ &cosme/k_login.php
@@ -0,0 +1,60 @@
+<?php session_start(); ?>
+<?php require 'db_connect.php'; ?>
+<?php
+$msg;
+if(isset($_POST['admin_email'])){
+    unset($_SESSION['admins']);
+    
+    $pdo = new PDO($connect, USER, PASS);
+    $sql = $pdo -> prepare('select * from Admins where admin_email=?');
+    $sql -> execute([$_POST['admin_email']]);
+    $password=$_POST['admin_password'];    
+    foreach($sql as $row){
+          if(password_verify($_POST['admin_password'],$row['admin_password'])==true){
+            $_SESSION['admins'] = [
+                'code' => $row['admin_code'],
+                'mail' => $row['admin_email'],
+                'pass' => $password];
+             }
+         }
+
+    if(isset($_SESSION['admins'])){
+        header('Location: ./k_home.php');
+        exit();
+    }else{
+        $msg = '<font color="FF0000">ログイン名またはパスワードが違います</font>';
+    }
+}
+require 'header.php';
+echo '<h3>&cosme</h3>';
+echo '<h2>ログイン</h2>';
+echo '<div id="hr2">';
+
+echo '<hr color="black">';
+echo '</div>';
+
+
+echo '<form action="k_login.php" method="post">';
+
+echo '<input type="text" name="admin_email" placeholder="メールアドレス"><br>';
+echo '<input type="password" name="admin_password"placeholder="パスワード">';
+
+if(isset($msg)){
+    echo '<p><div id="mannaka">', $msg, '</p></div>';
+}
+
+echo '<br>';
+echo '<p><button class="ao" type="submit" href="k_home.php">ログイン</button></p>';
+echo '</form>';
+echo '<div id="hr1">';
+echo '<hr width="250">';
+echo '</div>';
+echo '<br>';
+echo '<div id="mannaka">';
+echo '<p>アカウントをお持ちでない方はこちら</p>';
+echo '<br>';
+echo '<a href="k_member_new.php">新規登録</a>';
+echo '</div>';
+?>
+<?php require 'footer.php'; ?>
+]
---
+++ &cosme/k_member_check.php
@@ -0,0 +1,38 @@
+<?php session_start(); ?>
+<?php require 'db_connect.php'; ?>
+<?php
+ $_SESSION['admin'] = [
+    'mail' => $_POST['admin_email'],
+    'pass' => $_POST['admin_password']
+];
+$pdo=new PDO($connect,USER,PASS);
+$sql=$pdo->prepare('select * from Admins where admin_email=?');
+$sql->execute([$_POST['admin_email']]);
+if(!empty($sql->fetchAll())){
+    $_SESSION['admin']['mail']=null;
+    header('Location: ./k_member_new.php');
+    exit();
+}
+?>
+<?php require 'header.php'; ?>
+<h3>&cosme</h3>
+<div id="hr2"><hr color="black"></div>
+<div id="logtitle"><h2>登録確認</h2></div>
+
+    <form action="k_member_finish.php" method="post" id="next">
+        <?php
+        echo '<div id="meru2">';
+        echo '<input type="text" style="width: 240px;height: 27x;" name="admin_email" value="',$_POST['admin_email'], '" placeholder="',$_POST['admin_email'],'" readonly>';
+        echo '</div>';
+        echo '<br>';
+        echo '<div id="pas2">';
+        echo '<input type="text" style="width: 240px;height: 27x;" name="admin_password" value="',$_POST['admin_password'], '" placeholder="',$_POST['admin_password'],'" readonly>';
+        echo '</div>';
+        ?>
+    </form>
+    <br>
+    <button onclick="history.back()" class="grey">変更</button></p><br>
+    <button type="submit" form="next" class="ao">新規登録</button></p>
+</body>
+</html>
+<?php require 'footer.php'; ?>
