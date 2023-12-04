cdd26b83322d7b78b6c829838139d2f75d92b7c4
saekisora <148735028+saekisora@users.noreply.github.com>
2023-12-04 12:02:00 +0900

    a

--- &cosme/k_seles.php
+++ &cosme/k_seles.php
@@ -1,3 +1,4 @@
+<?php require 'db_connect.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,39 +9,87 @@
 <body>
     <h3>&cosme</h3>
     <hr>
-    <h3>売上管理画面</h3>
-    
-    <select name="uriage">
-        <option value="">2023/10/01~2023/10/07</option>
-        <option value="">2023/10/08~2023/10/14</option>
-        <option value="">2023/10/15~2023/10/21</option>
-        <option value="">2023/10/22~2023/10/28</option>
-        <option value="">2023/10/29~2023/11/04</option>
-    </select>
-
-    <table>
-        <tr>
-          <th>商品名</th>
-          <th>価格</th>
-          <th>売上数</th>
-        </tr>
-        <tr>
-          <td>商品名1</td>
-          <td>1000</td>
-          <td>10</td>
-        </tr>
-        <tr>
-          <td>商品名2</td>
-          <td>1000</td>
-          <td>20</td>
-        </tr>
-        <tr>
-            <td>商品名3</td>
-            <td>1000</td>
-            <td>30</td>
-          </tr>
-      </table>
-      <p><div class="ao"><button onclick="location.href='k_home.html'">ホームへ</button></div></p>
-
+    <h1>売上管理画面</h1>
+    <form action="k_seles.php" method="post">
+        <input type="date" name="min">～<input type="date" name="max">
+        <button type="submit">検索</button>
+    </form>
+    <table border="1">
+      <tr>
+          <th>売上日</th><th>商品名</th><th>カラー名</th><th>売上金額</th><th>売上数</th>
+      </tr>
+    <?php
+    $pdo=new PDO($connect, USER, PASS);
+    if(!empty($_POST['min'])&&!empty($_POST['max'])){
+        $sql=$pdo->prepare('select * from Orders where order_date between ? and ? order by order_date desc');
+        $sql->execute([$_POST['min'],$_POST['max']]);
+        foreach($sql as $row){
+            echo '<tr>';
+            $sql2=$pdo->prepare('select * from OrderDetails OD inner join Cosmetics C on OD.cosme_id = C.cosme_id where OD.order_id = ?');
+            $sql2->execute([$row['order_id']]);
+            foreach($sql2 as $row2){
+                echo '<td>',$row['order_date'],'</td>';
+                echo '<td>',$row2['cosme_name'],'</td>';
+                echo '<td>',$row2['color_name'],'</td>';
+                echo '<td>',$row2['price']*$row2['quantity'],'</td>';
+                echo '<td>',$row2['quantity'],'</td>';
+                echo '</tr>';
+            }
+            echo '</tr>';
+        }
+    }else if(!empty($_POST['min'])&&empty($_POST['max'])){
+        $sql=$pdo->prepare('select * from Orders where order_date >= ? order by order_date desc');
+        $sql->execute([$_POST['min']]);
+        foreach($sql as $row){
+            echo '<tr>';
+            $sql2=$pdo->prepare('select * from OrderDetails OD inner join Cosmetics C on OD.cosme_id = C.cosme_id where OD.order_id = ?');
+            $sql2->execute([$row['order_id']]);
+            foreach($sql2 as $row2){
+                echo '<td>',$row['order_date'],'</td>';
+                echo '<td>',$row2['cosme_name'],'</td>';
+                echo '<td>',$row2['color_name'],'</td>';
+                echo '<td>',$row2['price']*$row2['quantity'],'</td>';
+                echo '<td>',$row2['quantity'],'</td>';
+                echo '</tr>';
+            }
+            echo '</tr>';
+        }
+    }else if(empty($_POST['min'])&&!empty($_POST['max'])){
+        $sql=$pdo->prepare('select * from Orders where order_date <= ? order by order_date desc');
+        $sql->execute([$_POST['max']]);
+        foreach($sql as $row){
+            echo '<tr>';
+            $sql2=$pdo->prepare('select * from OrderDetails OD inner join Cosmetics C on OD.cosme_id = C.cosme_id where OD.order_id = ?');
+            $sql2->execute([$row['order_id']]);
+            foreach($sql2 as $row2){
+                echo '<td>',$row['order_date'],'</td>';
+                echo '<td>',$row2['cosme_name'],'</td>';
+                echo '<td>',$row2['color_name'],'</td>';
+                echo '<td>',$row2['price']*$row2['quantity'],'</td>';
+                echo '<td>',$row2['quantity'],'</td>';
+                echo '</tr>';
+            }
+            echo '</tr>';
+        }
+    }else{
+        $sql=$pdo->query('select * from Orders order by order_date desc');
+        foreach($sql as $row){
+            echo '<tr>';
+            $sql2=$pdo->prepare('select * from OrderDetails OD inner join Cosmetics C on OD.cosme_id = C.cosme_id where OD.order_id = ?');
+            $sql2->execute([$row['order_id']]);
+            foreach($sql2 as $row2){
+                echo '<td>',$row['order_date'],'</td>';
+                echo '<td>',$row2['cosme_name'],'</td>';
+                echo '<td>',$row2['color_name'],'</td>';
+                echo '<td>',$row2['price']*$row2['quantity'],'</td>';
+                echo '<td>',$row2['quantity'],'</td>';
+                echo '</tr>';
+            }
+            echo '</tr>';
+        }
+    }
+    ?>
+    </table>
+    <p><button onclick="location.href='k_home.html'">ホームへ</button></p>
 </body>
 </html>
