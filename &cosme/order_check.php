5dca83b29589ad94340e2360c66cc6e5dc8d4897
hirata0802 <148735103+hirata0802@users.noreply.github.com>
2023-12-04 13:32:08 +0900

    カート、オーダー

--- &cosme/order_check.php
+++ &cosme/order_check.php
@@ -3,14 +3,9 @@
 <?php require 'menu.php'; ?>
 <?php require 'db_connect.php'; ?>
     <h2>注文内容の確認</h2>
+<form action="order_finish.php" method="post">
 <?php
     $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('select sum(C.quantity * CO.price) as total from Cart as C inner join Cosmetics as CO on C.cosme_id=CO.cosme_id inner join Brands as B on CO.brand_id=B.brand_id where member_code=? and delete_flag=0');
-    $sql->execute([$_SESSION['customer']['code']]);
-    $total;
-    foreach($sql as $row){
-        $total=$row['total'];
-    }
     $sql=$pdo->prepare('select * from Addresses where member_code=? and register_date=(select max(register_date) from Addresses where member_code=?)');
     $sql->execute([$_SESSION['customer']['code'], $_SESSION['customer']['code']]);
 
@@ -21,11 +16,23 @@
         echo '〒', $row['post_code'], '<br>';
         echo $ads, '<br>';
         echo $row['phone'], '</dd>';
-        echo '支払い方法：', $_POST['test'];
-        echo '<dt>商品合計</dt><dd>', $total, '円</dd>';
+        echo '支払い方法：', $_POST['pay'];
+        echo '<dt>商品合計</dt><dd>', $_POST['total'], '円</dd>';
     }
     echo '<dl>';
+    //購入商品
+    $cart=$pdo->prepare('select * from Cart as C inner join Cosmetics as CO on C.cosme_id=CO.cosme_id where member_code=? and C.delete_flag=0');
+    $cart->execute([$_SESSION['customer']['code']]);
+    foreach($cart as $row){
+        //echo '<img src="', $row['image_path'], '" style="object-fit: contain; width: 100px; height: 100px;">';
+        echo $row['cosme_name'], '　';
+        echo 'カラー：', $row['color_name'];
+        echo $row['quantity'];
+    }
+    echo '<input type="hidden" name="pay" value="', $_POST['pay'], '">';
+    echo '<input type="hidden" name="total" value="', $_POST['total'], '">';
 ?>
-    <div class="ao"><button onclick="location.href='order_finish.php'">注文を確定する</button></div>
-    <div class="grey"><button onclick="location.href='cart.html'">変更</button></div>
+<button type="submit" class="ao">注文を確定する</button></p>
+</form>
+<button type="submit" onclick="location.href='order.php'" class="grey">変更</button></p><br>
 <?php require 'footer.php'; ?>
