a1c3795a083559959ce07dc34c635a3aec5bbe25
ShikimachiMinori <148735025+ShikimachiMinori@users.noreply.github.com>
2023-12-05 11:36:00 +0900

    Update order.php

--- &cosme/order.php
+++ &cosme/order.php
@@ -12,9 +12,7 @@
     foreach($sql as $row){
         $ads=$row['prefecture'].$row['city'].$row['section'].$row['building'];
         echo '<dl>';
-        echo '<div id="dai">';
         echo '<dt>お届け先</dt><dd>';
-        echo '</div>';
         echo '<div id="text1">';
         echo $row['address_name'], '　様<br>';
         echo '〒', $row['post_code'], '<br>';
@@ -32,9 +30,7 @@
     $total=$pdo->prepare('select sum(C.quantity * CO.price) as total from Cart as C inner join Cosmetics as CO on C.cosme_id=CO.cosme_id inner join Brands as B on CO.brand_id=B.brand_id where member_code=? and delete_flag=0');
     $total->execute([$_SESSION['customer']['code']]);
     foreach($total as $row){
-        echo '<div id="dai">';
         echo '<dt>商品合計</dt><dd>';
-        echo '</div>';
         echo '<div id="text1">';
         echo  $row['total'], '円</dd>';
         echo '</div>';
@@ -43,9 +39,7 @@
     echo '<input type="hidden" name="total" value="', $row['total'], '">';
 ?>
 <br>
-   <div id="dai">
     <dt>お支払い方法</dt>
-    </div>
     <dd>
     <div id="text1">
     <div class="radio-wrap">
