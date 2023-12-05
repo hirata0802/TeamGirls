47807d73674ef2dd95d3c30fed811387eb2bd82a
ShikimachiMinori <148735025+ShikimachiMinori@users.noreply.github.com>
2023-12-05 10:59:19 +0900

    Update order.php

--- &cosme/order.php
+++ &cosme/order.php
@@ -12,13 +12,16 @@
     foreach($sql as $row){
         $ads=$row['prefecture'].$row['city'].$row['section'].$row['building'];
         echo '<dl>';
-        echo '<dt>お届け先</dt><dd>', $row['address_name'], '　様<br>';
+        echo '<dt>お届け先</dt><dd>';
+        echo '<div id="mannaka">';
+        echo $row['address_name'], '　様<br>';
         echo '〒', $row['post_code'], '<br>';
         echo $ads, '<br>';
         echo $row['phone'], '</dd>';
         echo '<dd><div class="white"><button type="submit" onclick="location.href=`order_add.php`">お届け先追加</button></div></dd>';
         if($address->rowCount() > 1){
             echo '<dd><div class="white"><button type="submit" onclick="location.href=`order_change.php`">お届け先変更</button></div></dd>';
+            echo '</div>';
         }
     }
     echo '<br>';
@@ -26,7 +29,10 @@
     $total=$pdo->prepare('select sum(C.quantity * CO.price) as total from Cart as C inner join Cosmetics as CO on C.cosme_id=CO.cosme_id inner join Brands as B on CO.brand_id=B.brand_id where member_code=? and delete_flag=0');
     $total->execute([$_SESSION['customer']['code']]);
     foreach($total as $row){
-        echo '<dt>商品合計</dt><dd>', $row['total'], '円</dd>';
+        echo '<dt>商品合計</dt><dd>';
+        echo '<div id="mannaka">';
+        echo  $row['total'], '円</dd>';
+        echo '</div>';
     }
     echo '<form action="order_check.php" method="post">';
     echo '<input type="hidden" name="total" value="', $row['total'], '">';
@@ -34,11 +40,13 @@
 <br>
     <dt>お支払い方法</dt>
     <dd>
+    <div id="mannaka">
     <div class="radio-wrap">
     <input type="radio" name="pay" value="現金払い" required>現金払い<br>
     <input type="radio" name="pay" value="コンビニ払い">コンビニ払い<br>
     <input type="radio" name="pay" value="クレジットカード払い">クレジットカード払い<br>
     </div>
+</div>
     </dd>
     </dl>
     <br>
