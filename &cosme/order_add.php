178322f10ac99529b8967a6f3bde1fd9d2a00e0f
ShikimachiMinori <148735025+ShikimachiMinori@users.noreply.github.com>
2023-12-05 12:15:35 +0900

    Update order_add.php

--- &cosme/order_add.php
+++ &cosme/order_add.php
@@ -2,6 +2,7 @@
 <?php require 'menu.php'; ?>
     <button onclick="history.back()">＜戻る</button>
     <form action="order_db_insert.php" method="post">
+    echo '<div id="mannaka">';
     <input type="text" name="name" placeholder="お届け先氏名" required>
     <p><input type="text" name="zipcode" id="zipcode" placeholder="郵便番号" required>
         <!---↓検索ボタンで住所自動入力-->
@@ -13,6 +14,7 @@
     <p><input type="tel" name="tel" maxlength="11" pattern="^[0-9]+$" placeholder="電話番号" required></p>
     <input type="hidden" name="order" value="0">
     <button>追加</button>
+</div>
     </form>
     <script src="./js/jquery-3.7.0.min.js"></script>
     <script src="./js/app.js"></script>
